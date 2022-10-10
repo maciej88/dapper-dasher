46f14731a33bb255f5215803f580dca9de7396ff
maciej88 <mkrywult88@gmail.com>
2022-10-10 19:21:33 +0200

    velocity

--- dasher.cpp
+++ dasher.cpp
@@ -8,12 +8,23 @@ int main()
     // initialize window
     InitWindow(windowWidth, windowHeight, "Dapper Dasher");
 
+    // rectangle dimensions
+    const int width{50};
+    const int height{80};
+
+    int posY{windowHeight - height};
+    int velocity{-10};
+
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
         // start drawning
         BeginDrawing();
         ClearBackground(BLUE);
+
+        posY += velocity;
+
+        DrawRectangle(windowWidth/2, posY, width, height, WHITE);
 
         // stop drawning
         EndDrawing();
